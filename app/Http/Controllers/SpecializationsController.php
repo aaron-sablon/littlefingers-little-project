cac719f3df3525cfaaa877650d96dde346936b58
EmanManas <furioszona@gmail.com>
2019-05-07 12:25:31 -0700

    Update SpecializationsController.php, StudentsController.php and SubjectsController.php

--- app/Http/Controllers/SpecializationsController.php
+++ app/Http/Controllers/SpecializationsController.php
@@ -24,15 +24,48 @@ class SpecializationsController extends Controller
     }
 
     public function create(){
+    	$specializations = Specialization::all();
+        $this->params['specializations'] = $specializations;
+        //no route yet
+        return view('', $this->params);
+
 
     }
 
-    public function update(){
+    public function update(Request $request, $id){
+        $rules=Specialization::$rules;
+       $validator = Validator::make(
+           Input::all(),
+           $rules
+       );
+
+       // If validator fails.
+       if ( $validator->fails() ) {
+           
+           $error_messages = $validator->messages()->getMessages();
+           $this->params['error'] = true;
+           $this->params['msg'] = 'Form validation error. Please fix.';
+           $this->params['form_errors'] = $error_messages;
+
+           return redirect()->back()->with($this->params);
+       }
+       $specializations = Specialization::find($id);
+       $specializations->name =INPUT::get('name');
+       
+       $specializations->save();
+
+       $this->params['msg']='Information updated successfully.';
+       //no route yet
+       return redirect()->route('')->with($this->params);
     	
     }
 
-    public function destroy(){
+    public function destroy($id){
+    	$specializations = Specialization::find($id);
+        $specializations->delete();
 
+        $this->params['msg']='Specialization was removed successfully.';
+        //no route yet
+        return redirect()->route('')->with($this->params);
     }
-
 }
--- app/Http/Controllers/SubjectsController.php
+++ app/Http/Controllers/SubjectsController.php
@@ -24,14 +24,51 @@ class SubjectsController extends Controller
     }
 
     public function create(){
+        $subjects = Subject::all();
+        $this->params['subjects'] = $subjects;
+        //no route yet
+        return view('', $this->params);
 
     }
 
-    public function update(){
+    public function update(Request $request, $id){
+        $rules=Subject::$rules;
+        $validator = Validator::make(
+            Input::all(),
+            $rules
+        );
+
+        // If validator fails.
+        if ( $validator->fails() ) {
+            
+            $error_messages = $validator->messages()->getMessages();
+            $this->params['error'] = true;
+            $this->params['msg'] = 'Form validation error. Please fix.';
+            $this->params['form_errors'] = $error_messages;
+
+            return redirect()->back()->with($this->params);
+        }
+        $subjects = Subject::find($id);
+        $subjects->fname =INPUT::get('fname');
+        $subjects->lname =INPUT::get('lname');
+        $subjects->advisory=INPUT::get('advisory');
+        $subjects->contact=INPUT::get('contact');
+        
+        $subject->save();
+
+        $this->params['msg']='Information updated successfully.';
+        //no route yet
+        return redirect()->route('')->with($this->params);
     	
     }
 
-    public function destroy(){
+    public function destroy($id){
+        $subjects = Subject::find($id);
+        $subjects->delete();
+
+        $this->params['msg']='Subject was removed successfully.';
+        //no route yet
+        return redirect()->route('')->with($this->params);
 
     }
 
