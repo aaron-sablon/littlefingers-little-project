8ca6d20817380be7019347ace5798cdce8c36d11
sab1001 <aaron.codenbrew@gmail.com>
2019-05-09 11:06:13 +0800

    GIN KAY-O KO SALA NYO KAY BULAG KAMO

--- app/Http/Controllers/TimesController.php
+++ app/Http/Controllers/TimesController.php
@@ -24,14 +24,48 @@ class TimesController extends Controller
     }
 
     public function create(){
-
+        $times = Times::all();
+        $this->params['times'] = $times;
+        //no route yet
+        return view('', $this->params);
     }
 
-    public function update(){
-    	
+    public function update(Request $request, $id){
+    	$rules=Times::$rules;
+        $validator = Validator::make(
+            Input::all(),
+            $rules
+        );
+    
+        // If validator fails.
+        if ( $validator->fails() ) {
+            
+            $error_messages = $validator->messages()->getMessages();
+            $this->params['error'] = true;
+            $this->params['msg'] = 'Form validation error. Please fix.';
+            $this->params['form_errors'] = $error_messages;
+
+            return redirect()->back()->with($this->params);
+            
+        }
+        $times = Times::find($id);
+        $times->slot = INPUT::get('slot');
+
+        $times->save();
+
+        $this->params['msg']='Information updated successfully.';
+        //no route yet
+        return redirect()->route('')->with($this->params);
     }
 
-    public function destroy(){
+    public function destroy($id){
+        $times = Times::find($id);
+        $times->delete();
+
+        $this->params['msg']='Timeslot was removed successfully.';
+        //no route yet
+        return redirect()->route('')->with($this->params);
+
 
     }
 
