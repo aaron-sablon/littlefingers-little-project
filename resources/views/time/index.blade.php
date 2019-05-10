df1b021880acd00daf6aec48999048a1a7adc637
EmanManas <furioszona@gmail.com>
2019-05-09 19:34:45 -0700

    updated littlefingers-little-project\resources\views\professor, room, schedule, section, specialization, student, and subject

---
+++ resources/views/time/index.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.master')
+
+<div class="container col-md-10">
+        
+        <div class="container mt-4">
+    
+            <div class="container-fluid text-left my-3 py-2 border-top border-bottom border-success"> 
+                <div class="row">
+                    <ul class="list-inline mb-0">
+                        <li class="list-inline-item"> <h2>Time List</h2></li>
+                        <li class="list-inline-item"><h4 class="text-secondary">View Time</h4></li>
+                    </ul>
+                    <div class="col-sm-2  mt-4 ml-auto">
+                        <a  class="btn btn-info btn-block" href="{{route('')}}" role="button">View Students</a>
+                    </div>
+                </div>
+            </div>
+    
+            @if(
+                session()->has('msg') && session()->has('error') &&
+                session()->get('msg') && !session()->get('error')
+                )
+                <div class="alert alert-success">
+                    <p>{{ session()->get('msg') }}</p>
+                    <hr>
+                </div>
+             @endif
+    
+            <div>
+                <table class="table table-bordered">
+                    <thead class="text-center thead-light">
+                        <tr>
+                             <th></th>
+                            <th scope="col">Time</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach(  )
+                        <tr>
+                            <td scope="row" class="text-center" >
+                                <input class="form-check-input filled-in" type="checkbox" id="checkbox123">
+                                <label class="form-check-label" for="checkbox123" class="label-table"></label>
+                            </td>                    
+                            <td>
+                                <a href="{{ route('', ) }}">{{  }}</a>
+                            </td>
+                           
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+    
+            <div class="container">
+    
+                <div class="row">                   
+                    <div class="col-sm-2  mt-4">
+                        <a  class="btn btn-success btn-block" href="{{route('')}}" role="button">Add</a>
+                    </div>
+                     
+                    <div class="col-sm-10  mt-4">
+                        <div class="pagination justify-content-end">
+                            {{ () }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+    
+        </div>
+    </div>
---
+++ resources/views/time/update.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.master')
+
+<div class="container col-md-6">
+  
+  <div class="container mt-4">
+
+      <div class="container-fluid text-left my-2">     
+          <ul class="list-inline mb-1">
+              <li class="list-inline-item"> <h2>Room List</h2></li>
+              <li class="list-inline-item"><h4 class="text-secondary">Edit Room</h4></li>
+          </ul>
+      </div>
+
+    <!-- -->
+   
+      <div class="container">
+          <form action="{{ route('', ) }}" method="post" class="form-inline">
+          {{csrf_field()}}
+          @method('PUT')             
+              <div class="row border-bottom border-top border-success py-3 my-2">
+                 
+                 <div class="form-group col-sm-6">
+                        <label for="time" >Time:</label>
+                        <input type="text" class="form-control mb-2 ml-auto w-75" placeholder="Time" name="time" value="{{  }}">
+                 </div>
+                 
+              </div>
+
+              <div class="container">
+                  <div class="row my-3">
+                      <div class="col-sm-3">
+                          <a  href="{{ route('') }}" role="button" class="btn btn-primary btn-block">Back</a>
+                      </div>
+
+                      <div class="col-sm-3 ml-auto">
+                          <input type="submit" class="btn btn-success btn-block" name="editbtn" value="Save">
+                      </div>
+                  </div>
+              </div>
+          </form>
+      </div> 
+
+      @if(
+          session()->has('error') && session()->has('form_errors')
+          )
+          <div class="alert alert-danger">
+          @foreach(session()->get('form_errors') as $error)
+              <p>{{ $error[0] }}</p>
+              <hr>
+          @endforeach
+          </div>
+      @endif
+
+  </div>
+</div>
