58a78fad2bc9de628dd69add92e9685e73075edd
EmanManas <furioszona@gmail.com>
2019-05-16 14:53:28 -0700

    updated resoursces\views\specialization

--- resources/views/specialization/create.blade.php
+++ resources/views/specialization/create.blade.php
@@ -14,26 +14,47 @@
         <div class="container">
             <form action="{{route('specializations.store')}}" method="post" class="form-inline">
             {{csrf_field()}}
- 
-                <div class="row border-bottom border-top border-success py-3 my-2">
-                <div class="form-group col-sm-6 mt-3">
-                        <label for="gradelevel" class="mr-4">Grade level:</label>
-                        <select name="grade" id="service_type" class="custom-select ml-auto w-75" data-style="select-with-transition" title="Select Gradelevel" >
-                        
-                            <option value="7">Grade 7</option>
-                            <option value="8">Grade 8</option>
-                            <option value="9">Grade 9</option>
-                            <option value="10">Grade 10</option>
-                         
-                        </select>
+            <div class="row border-bottom border-top border-success py-3 my-2">
+            <div>
+                <table class="table table-bordered">
+                    <thead class="text-center thead-light">
+                        <tr>
+                            <th>Grade 7</th>
+                            <th>Grade 8</th>
+                            <th>Grade 9</th>
+                            <th>Grade 10</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td scope="row" class="text-center" >
+                                <input class="form-check-input filled-in" type="checkbox" id="checkbox123" value="Grade 7" name="grade">
+                                <label class="form-check-label" for="grade" class="label-table"></label>
+                            </td> 
+                            <td scope="row" class="text-center" >
+                                <input class="form-check-input filled-in" type="checkbox" id="checkbox123" value="Grade 8" name="grade">
+                                <label class="form-check-label" for="grade" class="label-table"></label>
+                            </td> 
+                            <td scope="row" class="text-center" >
+                                <input class="form-check-input filled-in" type="checkbox" id="checkbox123" value="Grade 9" name="grade">
+                                <label class="form-check-label" for="grade" class="label-table"></label>
+                            </td> 
+                            <td scope="row" class="text-center" >
+                                <input class="form-check-input filled-in" type="checkbox" id="checkbox123"  value="Grade 10" name="grade">
+                                <label class="form-check-label" for="grade" class="label-table"></label>
+                            </td> 
+                           
+                           
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="form-group col-sm-6 mt-3">
+                        <label for="name" class="mr-4">Name:</label>
+                        <input type="text" class="form-control mb-2 ml-auto w-75" placeholder="Specialization" name="name">
                     </div>
 
-                    <div class="form-group col-sm-6 mt-3">
-                        <label for="name" class="mr-4" >Name:</label>
-                        <input type="text" class="form-control mb-2 ml-auto w-75" placeholder="Name" name="name">
-                    </div>
-                </div>
-
+            </div>
                 <div class="container">
                     <div class="row my-3">
                         <div class="col-sm-3">
